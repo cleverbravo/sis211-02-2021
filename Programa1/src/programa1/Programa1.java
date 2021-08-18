6d7366aa1a67a5f34b95caf70a6f0da795820f78
clever matchless bravo villafuerte <cleveresclip@hotmail.com>
2021-08-18 09:19:43 -0400

    io basico

---
+++ Programa1/src/programa1/Programa1.java
@@ -0,0 +1,13 @@
+package programa1;
+import java.util.*;
+public class Programa1 {
+    public static void main(String[] args) {
+        Scanner cin=new Scanner(System.in);
+        System.out.print("Introduzca un numero entero:");
+        int a=cin.nextInt();
+        System.out.print("Introduzca un numero entero:");
+        int b=cin.nextInt();
+        int c=a+b;
+        System.out.println("Suma="+c);
+    }
+}
