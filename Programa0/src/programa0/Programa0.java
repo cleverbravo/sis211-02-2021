0f099c18f95ee2ef74c5b092d695bf66c24c48b6
clever matchless bravo villafuerte <cleveresclip@hotmail.com>
2021-08-18 09:09:26 -0400

    contenido y bibliografia

---
+++ Programa0/src/programa0/Programa0.java
@@ -0,0 +1,22 @@
+package programa0;
+
+public class Programa0 {
+    public static void main(String[] args) {
+        System.out.println("Contenido");
+        System.out.println("1.Introduccion");
+        System.out.println("2.Funciones y Clases");
+        System.out.println("3.Java (selectivas,repetitivas)");
+        System.out.println("4.Clases y Objetos");
+        System.out.println("5.Herencia  y Polimorfismo");
+        System.out.println("6.GUI");
+        System.out.println("7.Archivos");
+        System.out.println("8.Sockets");
+        System.out.println();System.out.println();
+        System.out.println("Bibliografía");
+        System.out.println("1.Programacion en Java, Joyanes");
+        System.out.println("2.Java 8, Deitel");
+        System.out.println("3.https://docs.oracle.com/en/java/javase/16/");
+        System.out.println("4.http://biblioteca.uatf.edu.bo/jspui/");
+        
+    }
+}
