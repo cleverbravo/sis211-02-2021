1d7df06137f9737f4fcbb4414537f8e1be66c456
clever matchless bravo villafuerte <cleveresclip@hotmail.com>
2021-08-27 17:06:23 -0400

    foreach

---
+++ programa10/src/programa10/Programa10.java
@@ -0,0 +1,10 @@
+package programa10;
+import java.util.*;
+public class Programa10 {
+    public static void main(String[] args) {
+        String []nombres=new String[]{"Carlos","clever","willian","aldrin","ariel","braian"};
+        for(String x:nombres){
+            System.out.println(x);
+        }
+    }
+}
